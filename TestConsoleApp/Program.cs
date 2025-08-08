1d2bc9fcab2a3246a524d9cfec058bda27e5f310
kristof <csorba.kristof@vik.bme.hu>
2025-08-08 15:03:07 +0200

    Solution

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var app = new Program(42);
+        }
+
+        private readonly int value;
+        public Program(int i)
+        {
+            this.value = i;
+        }
+    }
+}
